eca020aca3d14301fff45be1e4d6787d440ad376
zacmiller26 <zacmiller26@icloud.com>
2025-01-03 11:17:49 -0800

    feat(utils): add yearsSince function

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,19 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// @date format: yyyy-mm-dd
+export function yearsSince(date: string) {
+  const [year, month, day] = date.split('-').map(Number)
+  const now = new Date()
+  const years = now.getUTCFullYear() - year
+
+  if (
+    now.getUTCMonth() < month - 1 || // Adjust month to zero-based index
+    (now.getUTCMonth() === month - 1 && now.getUTCDate() < day)
+  ) {
+    return years - 1
+  }
+
+  return years
+}
