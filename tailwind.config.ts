7b0560d493b302899e7fe0f449ec1355df2e7ceb
zacmiller26 <zacmiller26@icloud.com>
2025-01-03 14:00:35 -0800

    fix(tailwind): include /ui/ directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/*.{js,ts,jsx,tsx,mdx}',
     './stories/**/*.{js,ts,jsx,tsx}',
     './.storybook/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}'
