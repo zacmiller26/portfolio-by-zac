ceb400279395c00193b2c0cdee50dfda95389bf8
zacmiller26 <zacmiller26@icloud.com>
2025-01-06 09:38:07 -0800

    chore: disable AppsWindow temporarily in site

--- app/components/windows/index.tsx
+++ app/components/windows/index.tsx
@@ -1,4 +1,4 @@
-import AppsWindow from '@/app/components/windows/AppsWindow'
+// import AppsWindow from '@/app/components/windows/AppsWindow'
 import ContactWindow from '@/app/components/windows/ContactWindow'
 import HomeWindow from '@/app/components/windows/HomeWindow'
 import SettingsWindow from '@/app/components/windows/SettingsWindow'
@@ -7,6 +7,6 @@ import { type WindowConfig } from '@/app/components/windows/types'
 export const windowConfigs: WindowConfig[] = [
   HomeWindow,
   ContactWindow,
-  AppsWindow,
+  // AppsWindow, TODO: Uncomment when ready to implement
   SettingsWindow
 ]
