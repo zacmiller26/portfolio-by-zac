fddcf8b8c2eba496e69b275b39d7661baee9431b
zacmiller26 <zacmiller26@icloud.com>
2025-01-06 09:34:20 -0800

    feat(settings): add PatternSelector and WallpaperSelector to SettingsWindow

--- app/components/windows/SettingsWindow/index.tsx
+++ app/components/windows/SettingsWindow/index.tsx
@@ -1,4 +1,6 @@
+import PatternSelector from '@/app/components/windows/SettingsWindow/PatternSelector'
 import ThemeSelector from '@/app/components/windows/SettingsWindow/ThemeSelector'
+import WallpaperSelector from '@/app/components/windows/SettingsWindow/WallpaperSelector'
 import { WindowConfig } from '@/app/components/windows/types'
 import { CogIcon } from 'lucide-react'
 
@@ -11,9 +13,15 @@ export const windowConfig: WindowConfig = {
 
 function SettingsWindow() {
   return (
-    <div className='space-y-4'>
+    <div className='space-y-6'>
       <p className='text-primary-1'>Theme</p>
       <ThemeSelector />
+
+      <p className='text-primary-1'>Pattern</p>
+      <PatternSelector />
+
+      <p className='text-primary-1'>Wallpaper</p>
+      <WallpaperSelector />
     </div>
   )
 }
