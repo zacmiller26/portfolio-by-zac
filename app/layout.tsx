a7362093ae1658158714c2a0b1fa62e81cca5797
zacmiller26 <zacmiller26@icloud.com>
2025-01-10 14:12:23 -0800

    feat(home): use const in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 
+import { FULL_NAME } from '@/lib/constants'
 import AppearanceProvider from '@/lib/contexts/appearance-context'
 import { cn } from '@/lib/utils'
 import { Analytics } from '@vercel/analytics/react'
@@ -17,8 +18,8 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'Zac Miller',
-  description: 'A web portfolio for Zac Miller.'
+  title: FULL_NAME,
+  description: `A web portfolio for ${FULL_NAME}.`
 }
 
 export default function RootLayout({
